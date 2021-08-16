8e27346d9c3b92398a52bc79864e695d670718d2
morg <morgan.guimard@gmail.com>
2021-08-16 20:51:08 +0200

    Add some comments in main cpp file.

--- OpenRGBSamplePlugin.cpp
+++ OpenRGBSamplePlugin.cpp
@@ -7,7 +7,16 @@ ResourceManager* OpenRGBSamplePlugin::RMPointer = nullptr;
 OpenRGBPluginInfo OpenRGBSamplePlugin::Initialize(bool Dt, ResourceManager *RM)
 {
     PInfo.PluginName         = "Sample plugin";
-    PInfo.PluginDescription  = "Allo allo";
+    PInfo.PluginDescription  = "Allo allo";       
+
+    /*-----------------------------------------------------*\
+    |     PInfo.PluginLocation                              |
+    |                                                       |
+    | InformationTab - Place plugin in the Information tab  |
+    | DevicesTab - Place plugin in the Devices tab          |
+    | TopTabBar - Place plugin as its own top level tab     |
+    | SettingsTabBar - Place plugin in the Settings tab     |
+    \*-----------------------------------------------------*/
     PInfo.PluginLocation     = "TopTabBar";
     PInfo.HasCustom          = true;
     PInfo.PluginLabel        = new QLabel("Sample plugin");
@@ -20,10 +29,11 @@ OpenRGBPluginInfo OpenRGBSamplePlugin::Initialize(bool Dt, ResourceManager *RM)
 
 QWidget* OpenRGBSamplePlugin::CreateGUI(QWidget* parent)
 {  
-    QWidget* widget =  new QWidget(nullptr);
+    // Retuning a dummy QWidget with a label
+    QWidget* widget =  new QWidget(parent);
+
     QHBoxLayout* layout = new QHBoxLayout();
     widget->setLayout(layout);
-
     layout->addWidget(new QLabel("Allo, allo?"));
 
     return widget;
